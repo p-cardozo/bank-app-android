13cc5ee94fd5ab473095b4feb3fd30515d94a63e
p-cardozo <contato.pcardozo@gmail.com>
2021-04-07 16:01:04 -0300

    subindo layout login

--- app/src/main/java/br/com/pcardozo/appbank/MainActivity.kt
+++ app/src/main/java/br/com/pcardozo/appbank/MainActivity.kt
@@ -2,10 +2,19 @@ package br.com.pcardozo.appbank
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.login_button_access).setOnClickListener {
+            Toast.makeText(this, "agora vaii", Toast.LENGTH_LONG).show()
+        }
     }
+
+
 }
